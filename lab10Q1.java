fb26adc0156f843ea62cfdc5b89a36ca8378f66e
Thomas Walsh <THOMAS.WALSH.2024@mumail.ie>
2023-12-11 09:46:48 +0000

    toedit

---
+++ lab10Q1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class lab10Q1 {
+    public static void main(String[]args)
+    Scanner scan = Scanner(System.in);
+    int square,circle,rectangle;
+    {
+        while(true)
+        {
+            System.out.println("Pick a shape");
+            String in=scan.nextLine();
+
+            if(in == "square")
+            {
+                square = l*w
+            }
+
+            if(in == "rectangle")
+            {
+                rectangle = l*w*h;
+            }
+            if(in == "circle")
+            {
+                circle = r*PI;
+            }
+
+        }
+    }
+}
