3a6493c17079b69ddd11bbded3d0ad360fff2db6
AbstractRain <109770015+AbstraktRain@users.noreply.github.com>
2023-12-12 13:54:33 +0000

    bfkw

---
+++ lab10Q2.java
@@ -0,0 +1,198 @@
+import java.util.Scanner;
+public class lab10Q2 {
+    public static void main(String[] args) { 
+
+Scanner scan = new Scanner(System.in);
+       System.out.println("Q2: Enter the current day (1-31): "); 
+        
+        int day = Integer.parseInt(scan.nextLine()); 
+        
+        System.out.println("Enter the current month: (1-12)"); 
+        
+        int month = Integer.parseInt(scan.nextLine()); 
+        
+         if (day == 1) 
+        
+         System.out.print("You selected 1st of "); 
+        
+        else if (day == 2) 
+        
+        System.out.print("You selected 2nd of "); 
+        
+       else if (day == 3) 
+      System.out.print("You selected 3rd of "); 
+        
+       else if (day == 4) 
+        
+       System.out.print("You selected 4th of "); 
+        
+       else if (day == 5) 
+        
+       System.out.print("You selected 5th of "); 
+        
+        else if (day == 6) 
+        
+        System.out.print("You selected 6th of "); 
+        
+       else if (day == 7) 
+        
+         System.out.print("You selected 7th of "); 
+        
+        else if (day == 8) 
+        
+         System.out.print("You selected 8th of "); 
+        
+        else if (day == 9) 
+        
+         System.out.print("You selected 9th of "); 
+        
+       else if (day == 10) 
+        
+        System.out.print("You selected 10th of "); 
+        
+        else if (day == 11) 
+        
+       System.out.print("You selected 11th of "); 
+         else if (day == 12) 
+        
+        System.out.print("You selected 12th of "); 
+        
+     else if (day == 13) 
+        
+        System.out.print("You selected 13th of "); 
+        
+     else if (day == 14) 
+        
+      System.out.print("You selected 14th of "); 
+        
+         else if (day == 15) 
+        
+        System.out.print("You selected 15th of "); 
+         else if (day == 16) 
+        
+     System.out.print("You selected 16th of "); 
+        
+         else if (day == 17) 
+        
+        System.out.print("You selected 17th of "); 
+        
+         else if (day == 18) 
+        
+      System.out.print("You selected 18th of "); 
+        
+    else if (day == 19) 
+         System.out.print("You selected 19th of "); 
+        
+       else if (day == 20) 
+        
+        System.out.print("You selected 20th of "); 
+        
+        else if (day == 21) 
+        
+        System.out.print("You selected 21st of "); 
+        
+         else if (day == 22) 
+        
+     System.out.print("You selected 22nd of "); 
+        
+       else if (day == 23) 
+        
+     System.out.print("You selected 23rd of "); 
+        
+        else if (day == 24) 
+        
+       System.out.print("You selected 24th of "); 
+        
+        else if (day == 25) 
+        
+        System.out.print("You selected 25th of "); 
+        
+      else if (day == 26) 
+        
+         System.out.print("You selected 26th of "); 
+        
+       else if (day == 27) 
+        
+         System.out.print("You selected 27th of "); 
+        
+        else if (day == 28) 
+        
+        System.out.print("You selected 28th of "); 
+        
+        else if (day == 29) 
+        
+        System.out.print("You selected 29th of "); 
+        
+         else if (day == 30) 
+        
+        System.out.print("You selected 30th of "); 
+        
+        else if (day == 31) 
+        
+        System.out.print("You selected 31st of "); 
+        
+         else { 
+        
+     System.out.println("Invalid day"); 
+        
+        } 
+        
+       if(month == 1) { 
+        
+       System.out.println("January"); 
+        
+        } 
+        else if (month == 2) { 
+        
+        System.out.println("February"); 
+    }
+
+     else if (month == 3) { 
+        
+       System.out.println("March"); 
+        
+         } 
+         else if (month == 4) { 
+        
+       System.out.println("April"); 
+        
+        } else if (month == 5) { 
+        
+   System.out.println("May"); 
+        
+         } else if (month == 6) { 
+        
+        System.out.println("June"); 
+        
+       } else if (month == 7) { 
+        
+         System.out.println("July"); 
+        
+         } else if (month == 8) { 
+        
+         System.out.println("Augst"); 
+        
+        } else if (month == 9) { 
+        
+        System.out.println("September"); 
+        
+         } else if (month == 10) { 
+        
+        System.out.println("October"); 
+        
+        } else if (month == 11) { 
+        
+         System.out.println("November"); 
+        
+         } else if (month == 12) { 
+        
+        System.out.println("December"); 
+        
+         } else { 
+        
+        System.out.println("Invalid month"); 
+        
+     } 
+        
+         } 
+}
